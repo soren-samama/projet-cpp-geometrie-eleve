6f7a8d240ce1717719e1557280d1b5c3c653fdd9
Carla THANG <carla.thang@universite-paris-saclay.fr>
2025-05-13 09:50:49 +0200

    modification du draw, equals et translate

--- src/shapes/circle.cpp
+++ src/shapes/circle.cpp
@@ -3,32 +3,34 @@
 #include <cmath>
 #include <numbers>
 #include <print>
+#include "draw.hpp"
 
 using namespace std; 
 
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
 
 double Circle::circumference() {
-	return 2 * numbers::pi * radius;
+	return 2 * numbers::pi * radius; //formule du périmètre d'un disque
 }
 
 double Circle::area() {
-	return numbers::pi * radius * radius;
+	return numbers::pi * radius * radius; //formule de l'aire d'un disque 
 }
 
 void Circle::draw() {
-    int i = 0;
-    while (i < 360) {
+	vector<Point>points; // on crée un vecteur de points pour stocker les points de coordonnées différentes
+    for(int i = 0 ; i<360; i++) { // on va faire varier un angle i pour tracer tous les points qui formeront le cercle
         double angle = i * numbers::pi / 180;
         double x = center.x + radius * cos(angle);
         double y = center.y + radius * sin(angle);
-        print(Circle.push_back(Point(x, y))); // pb avec le circle.push_back
+        points.push_back(Point(x,y)); 
     }
+	draw_picture(points); 
 }
 
 void Circle::translate(Point T) {
-    Circle.x = T.x + center.x; 
-    Circle.y = T.y + center.y; // circle n'est pas défini
+    center.x = T.x + center.x; 
+    center.y = T.y + center.y; 
 }
 
 
@@ -38,9 +40,6 @@ void Circle::resize(double ratio) {
 
 
 bool Circle::equals(Circle circle){
-    bool circle = false; 
-    if (r!==0){
-        circle = true; 
-    }
-     return circle; 
+    return(radius==circle.radius)&&(center.x==circle.center.x && center.y==circle.center.y);
 }
+
